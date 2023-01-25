b52e08fa5c495ad8af9133dbd11abde4e341d764
Konstantin <kosvadmail@atomlc.com.ua>
2023-01-25 19:54:13 +0100

    .

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,4 +1,3 @@
-/* eslint-disable node/no-unsupported-features/es-syntax */
 import { readFileSync } from 'fs';
 import { CitiesNames, offerType } from '../../const.js';
 import { OfferType } from '../../types/offer.js';
