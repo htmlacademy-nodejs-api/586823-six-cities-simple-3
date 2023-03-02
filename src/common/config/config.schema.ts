7ea8e4e4c1b62aa32ed182aa3b1fd01a39232ce0
Konstantin <kosvadmail@atomlc.com.ua>
2023-03-02 18:25:30 +0100

    test1

--- src/common/config/config.schema.ts
+++ src/common/config/config.schema.ts
@@ -11,8 +11,8 @@ export type ConfigSchema = {
   DB_PASSWORD: string;
   DB_PORT: number;
   DB_NAME: string;
-  UPLOAD_DIRECTORY: string;
   JWT_SECRET: string;
+  UPLOAD_DIRECTORY: string;
 }
 
 export const configSchema = convict<ConfigSchema>({
